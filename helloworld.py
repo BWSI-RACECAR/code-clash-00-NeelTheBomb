dc63218500ce6b1ba8b8b6d8a80bb96b4f222780
Neel <neelnarayan07@gmail.com>
2023-07-10 22:13:08 -0400

    added

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+class Solution:
+    def helloworld(self, string):
+        #input type: String
+
+        # TODO: Write code below to return a string with the solution to the prompt
+                # return type: String
+        if string == "Hello World!":
+            return "Hello World!"
+
+        pass
+
+def main():
+    tc1 = Solution()
+    string1= input()
+    ans = tc1.helloworld(string1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
